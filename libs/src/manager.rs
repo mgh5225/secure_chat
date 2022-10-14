8599fe87fbbf8cad20738096bc3e49b88940c8f8
Mohammad Ghazanfari <mgh.5225@gmail.com>
2022-10-14 22:38:42 +0330

    manager created

---
+++ libs/src/manager.rs
@@ -0,0 +1,27 @@
+mod PacketManager {
+    use crate::packet::{DataPacket, PacketError};
+
+    pub fn sendPacket(packet: DataPacket) -> Result<(), PacketError> {
+        todo!()
+    }
+
+    pub fn recvPacket() -> Result<DataPacket, PacketError> {
+        todo!()
+    }
+
+    pub fn signPacket(packet: DataPacket) -> Result<DataPacket, PacketError> {
+        todo!()
+    }
+
+    pub fn checkSignedPacket(packet: DataPacket) -> Result<(), PacketError> {
+        todo!()
+    }
+
+    pub fn encryptPacket(packet: DataPacket) -> Result<DataPacket, PacketError> {
+        todo!()
+    }
+
+    pub fn decryptPacket(packet: DataPacket) -> Result<DataPacket, PacketError> {
+        todo!()
+    }
+}
