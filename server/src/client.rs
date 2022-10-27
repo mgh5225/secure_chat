c2bf63292c327da2adac18a33afdbc56861722c5
Mohammad Ghazanfari <mgh.5225@gmail.com>
2022-10-27 12:36:18 +0330

    fix a major bug in server

--- server/src/client.rs
+++ server/src/client.rs
@@ -23,64 +23,82 @@ impl Client {
     }
 
     pub fn run(&mut self) {
-        let packet = packet_manager::recv_packet(&mut self.stream).unwrap();
+        loop {
+            let packet = match packet_manager::recv_packet(&mut self.stream) {
+                Ok(packet) => packet,
+                Err(err) => {
+                    println!("{}", err.to_string());
+                    break;
+                }
+            };
 
-        let packet: DataPacket = match packet.get_type() {
-            PacketType::PubKey => todo!(),
-            PacketType::Error => todo!(),
-            PacketType::Empty => todo!(),
-            PacketType::Ok => todo!(),
-            PacketType::E2E => match Packet::parse(&packet, "Packet Type Error E2E") {
-                Ok(packet) => self.start_e2e(packet),
-                Err(packet) => packet,
-            },
-            PacketType::Login => match Packet::parse(&packet, "Packet Type Error Login") {
-                Ok(packet) => self.login_user(packet),
-                Err(packet) => packet,
-            },
-            PacketType::Logout => {
-                self.me = None;
-                DataPacket::ok_message(String::from("Logout Successfully"))
-            }
-            PacketType::Register => match Packet::parse(&packet, "Packet Type Error Register") {
-                Ok(packet) => self.register_user(packet),
-                Err(packet) => packet,
-            },
-            PacketType::CreateGroup => {
-                match Packet::parse(&packet, "Packet Type Error CreateGroup") {
-                    Ok(packet) => self.create_group(packet),
+            let packet: DataPacket = match packet.get_type() {
+                PacketType::PubKey => todo!(),
+                PacketType::Error => continue,
+                PacketType::Empty => todo!(),
+                PacketType::Ok => todo!(),
+                PacketType::E2E => match Packet::parse(&packet, "Packet Type Error E2E") {
+                    Ok(packet) => self.start_e2e(packet),
                     Err(packet) => packet,
+                },
+                PacketType::Login => match Packet::parse(&packet, "Packet Type Error Login") {
+                    Ok(packet) => self.login_user(packet),
+                    Err(packet) => packet,
+                },
+                PacketType::Logout => {
+                    self.me = None;
+                    DataPacket::ok_message(String::from("Logout Successfully"))
                 }
-            }
-            PacketType::AddUser => match Packet::parse(&packet, "Packet Type Error AddUser") {
-                Ok(packet) => self.add_user(packet),
-                Err(packet) => packet,
-            },
+                PacketType::Register => {
+                    match Packet::parse(&packet, "Packet Type Error Register") {
+                        Ok(packet) => self.register_user(packet),
+                        Err(packet) => packet,
+                    }
+                }
+                PacketType::CreateGroup => {
+                    match Packet::parse(&packet, "Packet Type Error CreateGroup") {
+                        Ok(packet) => self.create_group(packet),
+                        Err(packet) => packet,
+                    }
+                }
+                PacketType::AddUser => match Packet::parse(&packet, "Packet Type Error AddUser") {
+                    Ok(packet) => self.add_user(packet),
+                    Err(packet) => packet,
+                },
 
-            PacketType::CreateMessage => {
-                match Packet::parse(&packet, "Packet Type Error CreateMessage") {
-                    Ok(packet) => self.create_message(packet),
-                    Err(packet) => packet,
+                PacketType::CreateMessage => {
+                    match Packet::parse(&packet, "Packet Type Error CreateMessage") {
+                        Ok(packet) => self.create_message(packet),
+                        Err(packet) => packet,
+                    }
                 }
-            }
-            PacketType::GetMessages => {
-                match Packet::<PacketModels::Empty>::parse(&packet, "Packet Type Error GetMessages")
-                {
-                    Ok(_) => self.get_messages(),
-                    Err(packet) => packet,
+                PacketType::GetMessages => {
+                    match Packet::<PacketModels::Empty>::parse(
+                        &packet,
+                        "Packet Type Error GetMessages",
+                    ) {
+                        Ok(_) => self.get_messages(),
+                        Err(packet) => packet,
+                    }
                 }
-            }
-            PacketType::GetChats => {
-                match Packet::<PacketModels::Empty>::parse(&packet, "Packet Type Error GetChats") {
-                    Ok(_) => self.get_chats(),
-                    Err(packet) => packet,
+                PacketType::GetChats => {
+                    match Packet::<PacketModels::Empty>::parse(
+                        &packet,
+                        "Packet Type Error GetChats",
+                    ) {
+                        Ok(_) => self.get_chats(),
+                        Err(packet) => packet,
+                    }
                 }
-            }
-            PacketType::Listen => todo!(),
-            _ => DataPacket::error_message(String::from("Packet Type Error")),
-        };
+                PacketType::Listen => todo!(),
+                _ => DataPacket::error_message(String::from("Packet Type Error")),
+            };
 
-        packet_manager::send_packet(&mut self.stream, packet).unwrap();
+            if let Err(err) = packet_manager::send_packet(&mut self.stream, packet) {
+                println!("{}", err.to_string());
+                break;
+            }
+        }
     }
 
     fn exchange_keys(&self) -> DataPacket {
